257c8b9b5e5caf9fbff1f985cfba00168ed58df1
Guillaume Huard <Guillaume.Huard@imag.fr>
2020-11-26 09:36:02 +0100

    Switch to python3

--- sleepproxyclient.py
+++ sleepproxyclient.py
@@ -12,7 +12,7 @@
 #
 # SleepProxyClient is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
@@ -33,6 +33,7 @@ import argparse
 import struct
 import subprocess
 import socket
+import codecs
 
 #
 #	A Python Wake On Demand client implementation (SleepProxyClient)
@@ -55,7 +56,11 @@ TTL_short = 120 # 2 min
 # debug flag
 DEBUG = False
 
-
+def debug(*args) :
+	if (DEBUG) :
+		for arg in args :
+			print(arg, end="")
+		print()
 
 def main() :
 
@@ -68,13 +73,12 @@ def main() :
 	if "all" in args.interfaces:
 		interfaces = sysIfaces
 
-	if (DEBUG) :
-		print "Interfaces: ", ", ".join(interfaces)
+	debug("Interfaces: ", ", ".join(interfaces))
 
 
 	for iface in interfaces :
 		if iface not in sysIfaces :
-			print "Invalid interface specified: ", iface
+			print ("Invalid interface specified: ", iface)
 		elif "lo" not in iface:
 			sendUpdateForInterface(iface)
 
@@ -82,8 +86,7 @@ def main() :
 
 def sendUpdateForInterface(interface) :
 # send update request per interface
-	if (DEBUG) :
-		print "-sendUpdateForInterface: ", interface
+	debug("-sendUpdateForInterface: ", interface)
 
 
 	# get IPs for given interface
@@ -99,11 +102,10 @@ def sendUpdateForInterface(interface) :
 			ipArr.append(ipEntry['addr'].split('%')[0]) # fix trailing %<iface>
 
 	if (len(ipArr) == 0) :
-		print "No IPv4 or IPv6 Addresses found for interface: ", interface
+		print ("No IPv4 or IPv6 Addresses found for interface: ", interface)
 		return
 
-	if (DEBUG) :
-		print "-sendUpdateForInterface: IPs: ", ", ".join(ipArr)
+	debug("-sendUpdateForInterface: IPs: ", ", ".join(ipArr))
 
 
 	# get HW Addr
@@ -112,14 +114,13 @@ def sendUpdateForInterface(interface) :
 	else:
 		hwAddr = ifaddrs[netifaces.AF_LINK][0]['addr']
 
-	if (DEBUG) :
-		print "-sendUpdateForInterface: HW-Addr: ", hwAddr
+	debug("-sendUpdateForInterface: HW-Addr: ", hwAddr)
 
 
 	# get all available sleep proxies
 	proxy = discoverSleepProxyForInterface(interface)
 	if (proxy == False) :
-		print "No sleep proxy available for interface: ", interface
+		print ("No sleep proxy available for interface: ", interface)
 		return
 
 
@@ -127,8 +128,7 @@ def sendUpdateForInterface(interface) :
 	host = socket.gethostname()
 	host_local = host + ".local"
 
-	if (DEBUG) :
-		print "-sendUpdateForInterface: Host: " + host_local
+	debug("-sendUpdateForInterface: Host: " + host_local)
 
 
 	# create update request
@@ -136,39 +136,36 @@ def sendUpdateForInterface(interface) :
 
 	## add some host stuff
 	for currIP in ipArr :
-	
 		ipVersion = dns.inet.af_for_address(currIP)
-	
+
 		if ipVersion == dns.inet.AF_INET:
 			dnsDatatype = dns.rdatatype.A
 		elif ipVersion == dns.inet.AF_INET6:
 			dnsDatatype = dns.rdatatype.AAAA
 		else :
 			continue
-	
+
 		update.add(dns.reversename.from_address(currIP), TTL_short, dns.rdatatype.PTR, host_local)
-		update.add(host_local, TTL_short, dnsDatatype,  currIP)
+		update.add(host_local, TTL_short, dnsDatatype,	currIP)
 
 	## add services
 	for service in discoverServices(ipArr) :
 
-		service_type = service[0] + ".local"
+		service_type = service[0].decode() + ".local"
 		service_type_host = host + "." + service_type
-		port = service[1]
+		port = service[1].decode()
 
 		# add the service
-	 	txtrecord = ""
-		if (len(service) == 2 or service[2] == "") :
-			txtrecord = chr(0)
+		if ((len(service) == 2 or service[2] == b"") and not DEBUG) :
+			update.add(service_type_host, TTL_long, dns.rdatatype.TXT)
 		else :
+			txtrecord = ""
 			for i in range(2,len(service)) :
-					txtrecord += " " + service[i]
+					txtrecord += " " + service[i].decode()
+			if (DEBUG) :
+				txtrecord += " SPC_STATE=sleeping"
 
-		if (DEBUG) :
-			txtrecord += " SPC_STATE=sleeping"
-
-
-		update.add(service_type_host, TTL_long, dns.rdatatype.TXT, txtrecord)
+			update.add(service_type_host, TTL_long, dns.rdatatype.TXT, txtrecord)
 
 		# device-info service gets a txt record only
 		if (service_type != "device-info._tcp.local") :
@@ -185,60 +182,55 @@ def sendUpdateForInterface(interface) :
 
 	# http://tools.ietf.org/id/draft-cheshire-edns0-owner-option-00.txt
 	# 4: edns owner option (MAC addr for WOL Magic packet)
-	cleanMAC = hwAddr.replace(":", "")
-	ownerOption = dns.edns.GenericOption(4, ("0000" + cleanMAC).decode('hex_codec'))
+	cleanMAC = "0000" + hwAddr.replace(":", "")
+	ownerOption = dns.edns.GenericOption(4, codecs.decode(cleanMAC, 'hex_codec'))
 
 	update.use_edns(edns=True, ednsflags=TTL_long, options=[leaseTimeOption, ownerOption])
 
-	if (DEBUG) :
-		print "-sendUpdateForInterface: request: ", update
-
+	debug("-sendUpdateForInterface: request: ", update)
 
 	# send request to proxy
 	try:
-		if (DEBUG) :
-			print "-sendUpdateForInterface: sending update to " + proxy['ip']
+		debug("-sendUpdateForInterface: sending update to ", proxy['ip'])
 
 		response = dns.query.udp(update, proxy['ip'], timeout=10, port=int(proxy['port']))
 
-		if (DEBUG) :
-			print "-sendUpdateForInterface: response: ", response
+		debug("-sendUpdateForInterface: response: ", response)
 
 		rcode = response.rcode()
 		if rcode != dns.rcode.NOERROR:
-			print "Unable to register with SleepProxy " + proxy['name'] + " (" + proxy['ip'] + ":" + proxy['port'] + ") - Errcode: " + rcode
-			print response
+			print ("Unable to register with SleepProxy " + proxy['name'] + " (" + proxy['ip'] + ":" + proxy['port'] + ") - Errcode: " + rcode)
+			print (response)
 
-	except DNSException, e:
-		print "Unable to register with SleepProxy " + proxy['name'] + " (" + proxy['ip'] + ":" + proxy['port'] + ")"
-		print e.__class__, e
+	except (DNSException, e):
+		print ("Unable to register with SleepProxy " + proxy['name'] + " (" + proxy['ip'] + ":" + proxy['port'] + ")")
+		print (e.__class__, e)
 
 
 
 def discoverServices(ipArray) :
 # discover all currently announced services from given IPs
-	if (DEBUG) :
-		print "-discoverServices: IPs: " + ", ".join(ipArray)
+	debug("-discoverServices: IPs: " + ", ".join(ipArray))
 
 	services = []
 	cmd = "avahi-browse --all --resolve --parsable --no-db-lookup --terminate 2>/dev/null | grep '^=;'"
 
 	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	for line in p.stdout.readlines() :
-		lineArr = line.rsplit(";")
+		lineArr = line.rsplit(b";")
 
 		# check length
 		if (len(lineArr) < 10) :
 			p.terminate()
-			print "discovering services failed for: " + str(ipArray)
+			print ("discovering services failed for: " + str(ipArray))
 			break
 
 		else :
 			#extract service details
-			if (lineArr[7] in ipArray) :
+			if (lineArr[7].decode() in ipArray) :
 				service = lineArr[4]
 				port = lineArr[8]
-				txtRecords = lineArr[9].replace('" "', ';').replace('\n', '').replace('"', '').rsplit(';')
+				txtRecords = lineArr[9].replace(b'" "', b';').replace(b'\n', b'').replace(b'"', b'').rsplit(b';')
 				serviceEntry = [service, port] + txtRecords
 				if (serviceEntry not in services) : # check for duplicates due to IPv4/6 dual stack
 					services.append(serviceEntry)
@@ -246,8 +238,7 @@ def discoverServices(ipArray) :
 	# wait for cmd to terminate
 	p.wait()
 
-	if (DEBUG) :
-		print "-discoverServices: discovered Services: ", services
+	debug("-discoverServices: discovered Services: ", services)
 
 	return services
 
@@ -255,8 +246,7 @@ def discoverServices(ipArray) :
 
 def discoverSleepProxyForInterface(interface) :
 
-	if (DEBUG) :
-		print "-discoverSleepProxyForInterface: Interface: ", interface
+	debug("-discoverSleepProxyForInterface: Interface: ", interface)
 
 	cmd = "avahi-browse --resolve --parsable --no-db-lookup --terminate _sleep-proxy._udp 2>/dev/null | grep '^=;" + interface.rsplit(":")[0] + "'"
 
@@ -267,8 +257,9 @@ def discoverSleepProxyForInterface(interface) :
 
 	# get all sleep proxies for the given interface an check for duplicates (IPv4/IPv6)
 	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	debug("-discoverSleepProxyForInterface: avahi-browse launched as PID ", p.pid)
 	for line in p.stdout.readlines() :
-		lineArr = line.rsplit(";")
+		lineArr = line.rsplit(b";")
 
 		# check length
 		if (len(lineArr) < 10) :
@@ -277,10 +268,9 @@ def discoverSleepProxyForInterface(interface) :
 
 
 		currProxy = { "name" : lineArr[6], "ip" : lineArr[7], "port" : lineArr[8] }
-		properties = lineArr[3].rsplit(" ")[0]
+		properties = lineArr[3].rsplit(b" ")[0]
 
-		if (DEBUG) :
-			print "-discoverSleepProxyForInterface: available proxy: ", currProxy, " with properties: ", properties
+		debug("-discoverSleepProxyForInterface: available proxy: ", currProxy, " with properties: ", properties)
 
 		# choose the server with lowest properties and prefer none 169.254.X.X addresses
 		if (minProperties == "" or minProperties > properties or (proxy and proxy['ip'].startswith('169.254.'))):
@@ -290,8 +280,7 @@ def discoverSleepProxyForInterface(interface) :
 	# wait for cmd to terminate
 	p.wait()
 
-	if (DEBUG) :
-		print "-discoverSleepProxyForInterface: selected proxy: ", proxy, " with properties: ", minProperties
+	debug("-discoverSleepProxyForInterface: selected proxy: ", proxy, " with properties: ", minProperties)
 
 	return proxy
 
